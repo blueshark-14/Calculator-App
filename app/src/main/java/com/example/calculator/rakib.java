9a5f83bca4965d9d30a157d5047686d6272d744b
blueshark-14 <rakibhasan1107@gmail.com>
2021-11-16 22:56:36 +0600

    created rakib class

---
+++ app/src/main/java/com/example/calculator/rakib.java
@@ -0,0 +1,17 @@
+package com.example.calculator;
+
+public class rakib {
+    int x;
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public rakib(int x) {
+        this.x = x;
+    }
+}
